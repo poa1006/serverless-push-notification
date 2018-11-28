f56fa3fe3492ad88ce31a91e665f3934820e2c36
Po <poc2@student.unimelb.edu.au>
2018-11-28 23:13:55 +1100

    Add registration

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
-console.log("Hello world");
+console.log('Hello world');
+
+
+
+if (navigator.serviceWorker) {
+  console.log('serviceWorker first try');
+  registerPush('Csy0uWHebU3pw--NmWa09ZoSgT7bHdB_J7PQEPjj-MlfaFVxekh8a4SmBD8VLssY43XwdO1Tz2S9rStKX_dtfo');
+}
+
+function registerPush(appPubkey) {
+  navigator.serviceWorker.register('service-worker.js');
+  navigator.serviceWorker.ready.then(function(registration) {
+      return registration.pushManager.getSubscription()
+          .then(function(subscription) {
+              console.log(JSON.stringify({ subscription: subscription }));
+              if (subscription) {
+                  return subscription;
+              }
+
+              return registration.pushManager.subscribe({
+                  userVisibleOnly: true,
+                  applicationServerKey: urlBase64ToUint8Array(appPubkey)
+              });
+          }) 
+          .then(function(subscription) {
+              console.log(JSON.stringify({ subscription: subscription }));
+          });
+  });
+}
+
+function urlBase64ToUint8Array(base64String) {
+  var padding = '='.repeat((4 - base64String.length % 4) % 4);
+  var base64 = (base64String + padding)
+      .replace(/\-/g, '+')
+      .replace(/_/g, '/');
+
+  var rawData = window.atob(base64);
+  var outputArray = new Uint8Array(rawData.length);
+
+  for (var i = 0; i < rawData.length; ++i)  {
+      outputArray[i] = rawData.charCodeAt(i);
+  }
+
+  return outputArray;
+}
